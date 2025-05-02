4c190cb830b196192e88d78cec6de8a8058aa457
atlinx <atlinuxtm@gmail.com>
2025-05-02 03:29:50 -0400

    WIP

---
+++ game/scripts/pixel_contestant.gd
@@ -0,0 +1,14 @@
+class_name PixelContestant
+extends CharacterBody2D
+
+
+@export var speed: float = 100
+@export var angle_jitter: Vector2 = Vector2(-10, 10)
+
+var direction = Vector2.RIGHT
+
+
+func _physics_process(delta: float) -> void:
+	var collision = move_and_collide(direction * speed * delta)
+	if collision:
+		direction = direction.bounce(collision.get_normal()).rotated(deg_to_rad(randf_range(angle_jitter.x, angle_jitter.y)))
